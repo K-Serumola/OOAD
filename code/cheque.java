750579039bb134301efc8da99ae0deec1f89eff5
K-Serumola <cse24-008@thuto.bac.ac.bw>
2025-10-07 09:49:40 +0200

    Add files via upload
    eish

---
+++ code/cheque.java
@@ -0,0 +1,9 @@
+public class cheque extends account implements withdrawable{
+
+    //const
+    public cheque(int accountNo,double balance, String branch){
+        super(accountNo,balance,branch);    //calls super class constructor
+    }
+
+
+}
---
+++ code/savings.java
@@ -0,0 +1,8 @@
+public class savings extends account implements interest {
+    float interesRate;
+
+    public savings(int accountNo,double balance, String branch, float interesRate) {
+        super(accountNo,balance,branch);
+        this.interesRate=interesRate;
+    }
+}
