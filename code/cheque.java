75010e1c47b9314d6b908c76fa56df9ea639e548
K-Serumola <cse24-008@thuto.bac.ac.bw>
2025-10-08 21:53:16 +0200

    Delete code directory
    so i can fix code structure

--- code/cheque.java
+++
@@ -1,9 +0,0 @@
-public class cheque extends account implements withdrawable{
-
-    //const
-    public cheque(int accountNo,double balance, String branch){
-        super(accountNo,balance,branch);    //calls super class constructor
-    }
-
-
-}
--- code/investments.java
+++
@@ -1,8 +0,0 @@
-public class investments extends account implements withdrawable, interest {
-    float interesRate;
-
-    public investments(int accountNo,double balance, String branch, float interesRate) {
-        super(accountNo,balance,branch);
-        this.interesRate=interesRate;
-    }
-}
