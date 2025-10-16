16116ce28b8114667d00d0ca3c94a479aedab2e8
K-Serumola <cse24-008@thuto.bac.ac.bw>
2025-10-16 08:48:27 +0200

    repo structure fix

---
+++ OOADassignment/src/main/java/com/demo/ooadassignment/HelloController.java
@@ -0,0 +1,14 @@
+package com.demo.ooadassignment;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
