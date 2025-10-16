5cea7ef04c21709864a40ab449e869a8d11eb444
K-Serumola <cse24-008@thuto.bac.ac.bw>
2025-10-16 08:47:11 +0200

    deleate old structure

--- src/savings.java
+++
@@ -1,8 +0,0 @@
-public class savings extends account implements interest {
-    float interesRate;
-
-    public savings(int accountNo,double balance, String branch, float interesRate) {
-        super(accountNo,balance,branch);
-        this.interesRate=interesRate;
-    }
-}
